3a018f9acfaa4e1beab685ee3b061a80a2be1f17
Daniel Münch <dani.munch@gmail.com>
2015-09-10 23:25:36 +0200

    Add class GeoCoord and its specs.

---
+++ lib/geocoord.rb
@@ -0,0 +1,12 @@
+class GeoCoord
+  attr_reader :lat
+  attr_reader :long
+	
+  def initialize(coord_as_string)
+    match = /^([-+]?[0-9]+(?:\.[0-9]*)?),([-+]?[0-9]+(?:\.[0-9]*)?){1}$/.match(coord_as_string.strip)
+    raise ArgumentError.new('Invalid coordinate string format') if match.nil?
+    
+    @lat = match[1].to_f
+    @long = match[2].to_f
+  end
+end	
---
+++ spec/geocoord_spec.rb
@@ -0,0 +1,36 @@
+require 'geocoord'
+
+RSpec.describe GeoCoord do
+  context "given valid geocoordinate in string format" do
+    it "parses it correctly" do
+      coord = GeoCoord.new("12.2,13.4")
+      expect(coord.lat).to eq 12.2 
+      expect(coord.long).to eq 13.4 
+    end
+    it "allows integer coordinates" do
+      coord = GeoCoord.new("1,13.4")
+      expect(coord.lat).to eq 1 
+      expect(coord.long).to eq 13.4 
+    end
+    it "allows negative and positive values" do
+      coord = GeoCoord.new("+1,-13.4")
+      expect(coord.lat).to eq 1 
+      expect(coord.long).to eq -13.4 
+    end
+    it "allows whitespace in the beginning and the end" do
+      coord = GeoCoord.new("+1,-13.4 ")
+      expect(coord.lat).to eq 1 
+      expect(coord.long).to eq -13.4 
+    end
+  end
+  context "invalid string formats are rejected" do
+    it "raises if longitude is missing 12.2," do
+      expect { GeoCoord.new("12.2,") }.to raise_error(ArgumentError, "Invalid coordinate string format")
+    end
+    it "raises if latitude is missing ,13.3" do
+      expect { GeoCoord.new(",13.3") }.to raise_error(ArgumentError, "Invalid coordinate string format")
+    end
+  end
+end
+      
+    
