c9b05dab07c65d56ea7c102fece35edbe22b4ebd
Daniel Münch <dani.munch@gmail.com>
2015-09-11 08:24:48 +0200

    car_api: Don't use inline rescue and group specs into contexts.

--- lib/car_api.rb
+++ lib/car_api.rb
@@ -3,10 +3,11 @@ require 'geocoord'
 
 get '/cars' do
   location = params['location']
-  
   halt 400, {'Content-Type' => 'text/plain'}, 'location parameter missing' unless location
 
-  coord = GeoCoord.new(location) rescue nil
-  
-  halt 400, {'Content-Type' => 'text/plain'}, 'location parameter malformed' unless coord 
+  begin
+    coord = GeoCoord.new(location) 
+  rescue ArgumentError => error
+    halt 400, {'Content-Type' => 'text/plain'}, error.message 
+  end
 end
--- spec/car_api_spec.rb
+++ spec/car_api_spec.rb
@@ -4,30 +4,33 @@ require 'car_api'
 require 'rspec'
 require 'rack/test'
 
-describe "car_api" do
+RSpec.describe "car_api" do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
 
-  it "refuses requests without location parameter with http status 400, bad request" do
-    get '/cars'
-    #expect(last_response).to be_ok 
-    expect(last_response.status).to eq 400 
-  end
+  context "not given a location parameter" do
+    it "refuses request with http status 400, bad request" do
+      get '/cars'
+      expect(last_response.status).to eq 400 
+    end
+    
+    it "refuses requests approbiate error message" do
+      get '/cars'
+      expect(last_response.body).to eq "location parameter missing" 
+    end
+  end  
   
-  it "refuses requests without location parameter with approbiate error message" do
-    get '/cars'
-    expect(last_response.body).to eq "location parameter missing" 
-  end
-  
-  it "refuses requests with malformed location parameter with http status 400, bad request" do
-    get '/cars?location=12.3,'
-    expect(last_response.status).to eq 400 
-  end
-  it "refuses requests with malformed location parameter with approbiate error message" do
-    get '/cars?location=12.3,'
-    expect(last_response.body).to eq "location parameter malformed" 
+  context "given a malformed location parameter" do
+    it "refuses requests with http status 400, bad request" do
+      get '/cars?location=12.3,'
+      expect(last_response.status).to eq 400 
+    end
+    it "refuses requests approbiate error message" do
+      get '/cars?location=12.3,'
+      expect(last_response.body).to eq "Invalid coordinate string format" 
+    end
   end
 end
